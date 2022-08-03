46fd1e27622dcacc32274bd63db8111501464836
MohammadAli Ebrahimzadeh <Mohammadali.Ebrahimzadeh@samansalamat.net>
2022-08-03 11:27:02 +0430

    Add DbContext

---
+++ CustomDrink.Web/DependencyInjectionExtention.cs
@@ -0,0 +1,14 @@
+﻿using CustomDrink.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomDrink.Web
+{
+    internal static class DependencyInjectionExtention
+    {
+        internal static IServiceCollection InjectContext(this IServiceCollection services) =>
+            services.AddDbContext<CustomDrinkContext>(options =>
+            {
+                options.UseInMemoryDatabase(databaseName: "InMem");
+            });
+    }
+}
--- CustomDrink.Web/Program.cs
+++ CustomDrink.Web/Program.cs
@@ -1,3 +1,5 @@
+using CustomDrink.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Dependency
+
+builder.Services
+    .InjectContext();
+
+#endregion
 
 var app = builder.Build();
 
