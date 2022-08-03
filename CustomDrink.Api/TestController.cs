597df0089feb63d3927e5738211458ec11106aa5
MohammadAli Ebrahimzadeh <Mohammadali.Ebrahimzadeh@samansalamat.net>
2022-08-03 15:27:12 +0430

    Fix Swagger

---
+++ CustomDrink.Api/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomDrink.Common.Generators;
+
+
+namespace CustomDrink.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Index/{id?}")]
+        public async Task<IActionResult> Index(string id)
+        {
+            var test = id.GenerateHash();
+
+            return Ok(test);
+        }
+    }
+}
---
+++ CustomDrink.Common/Generators/HashGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+namespace CustomDrink.Common.Generators
+{
+    public static class HashGenerator
+    {
+        public static string GenerateHash(this string plainText)
+        {
+
+            HashAlgorithm algorithm = SHA512.Create();
+
+            var hash =
+                algorithm.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
